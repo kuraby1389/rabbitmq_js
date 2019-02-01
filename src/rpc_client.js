4aa4e69f93f52fe5b9e5f42fde10b5320c3299dc
chiamtc <chiamcheng@gmail.com>
2019-02-01 15:34:01 +1000

    some update from reading regarding to saga pattern to achieve atomicity of dabtase

--- src/rpc_client.js
+++ src/rpc_client.js
@@ -9,22 +9,35 @@ if (args.length === 0) {
     process.exit(1);
 }
 
+
+/*according to this article : https://dzone.com/articles/saga-pattern-how-to-implement-business-transaction-1
+saga pattern tips
+a. Create a Unique ID per Transaction
+b. Add the Reply Address Within the Command
+c. Idempotent Operations
+d. Avoiding Synchronous Communications
+so far, I think this code from rabbitmq tuts cover some basis.
+*/
+
 amqp.connect('amqp://localhost', function (err, conn) {
     conn.createChannel(function (err, ch) {
-        ch.assertQueue('', {exclusive: true}, function (err, q) {
-            var corr = generateUuid();
+        //step 1
+        ch.assertQueue('', {exclusive: true}, function (err, q) {               //d. start - async all the way down to ch.consume(...)
+            var corr = generateUuid();          //a. unique id generated
             var num = parseInt(args[0]);
 
             console.log(' [x] Requesting fib(%d)', num);
             console.log('q.queue', q.queue);
 
-
-            ch.sendToQueue('rpc_queue',
+            //step 2
+            ch.sendToQueue('rpc_queue',                             //c. idempotent operations so far (obviously, should be imported from a list of CONSTANT.js file
                 new Buffer(num.toString()),
-                {correlationId: corr, replyTo: q.queue});
+                {correlationId: corr, replyTo: q.queue});           //b. always have replyTo to address which req and res to communicate
 
-
+            //step 5
             ch.consume(q.queue, function (msg) {
+                console.log('msg',msg)
+                //step 6 - to check correlationId generated in step 1 to see if it's the right process.
                 if (msg.properties.correlationId === corr) {
                     console.log(' [.] Got %s', msg.content.toString());
                     setTimeout(function () {
--- src/rpc_server.js
+++ src/rpc_server.js
@@ -2,32 +2,45 @@
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var q = 'rpc_queue';
+/*according to this article : https://dzone.com/articles/saga-pattern-how-to-implement-business-transaction-1
+saga pattern tips
+a. Create a Unique ID per Transaction
+b. Add the Reply Address Within the Command
+c. Idempotent Operations
+d. Avoiding Synchronous Communications
+so far, I think this code from rabbitmq tuts cover some basis.
+*/
 
-    ch.assertQueue(q, {durable: false});
-    ch.prefetch(1);
-    console.log(' [x] Awaiting RPC requests');
-    ch.consume(q, function reply(msg) {
-      var n = parseInt(msg.content.toString());
+amqp.connect('amqp://localhost', function (err, conn) {
+    conn.createChannel(function (err, ch) {
+        var q = 'rpc_queue';                                //c. idempotent operations so far (obviously, should be imported from a list of CONSTANT.js file
 
-      console.log(" [.] fib(%d)", n);
+        ch.assertQueue(q, {durable: false});
+        ch.prefetch(1);
+        console.log(' [x] Awaiting RPC requests');
 
-      var r = fibonacci(n);
+        //step 3
+        ch.consume(q, function (msg) {                      //d. start - async all the way down to ch.sendToQueue(...)
+            console.log('msg', msg)
+            var n = parseInt(msg.content.toString());
 
-      ch.sendToQueue(msg.properties.replyTo,
-        new Buffer(r.toString()),
-        {correlationId: msg.properties.correlationId});
+            console.log(" [.] fib(%d)", n);
 
-      ch.ack(msg);
+            var r = fibonacci(n);
+
+            //step 4
+            ch.sendToQueue(msg.properties.replyTo,
+                new Buffer(r.toString()),
+                {correlationId: msg.properties.correlationId});     //unique uuid from rpc_client.js
+
+            ch.ack(msg);
+        });
     });
-  });
 });
 
 function fibonacci(n) {
-  if (n === 0 || n === 1)
-    return n;
-  else
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    if (n === 0 || n === 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
